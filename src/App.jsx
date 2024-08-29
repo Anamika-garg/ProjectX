f99d7708c046b28b90951af0805b75ad8743e9bc
Anamika Garg <anamikagarg2906@gmail.com>
2024-08-29 19:46:42 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ const App = () => {
 
     <div>
       <Home />
-      <Overview />
+      {/* <Overview /> */}
       <Features />
       <Tools />
       <Faq />
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -15,8 +15,8 @@ const Header = () => {
                 <li><a href="#home"><img src={logo} alt="" height={'auto'} width={40} /></a></li>
                 <li className='hoverEffect active'><a href="#overview">Overview</a></li>
                 <li className='hoverEffect'><a href="#features">Features</a></li>
-                <li className='hoverEffect'><a href="#tools">Tools</a></li>
-                <li className='hoverEffect'><a href="#faq">FAQs</a></li>
+                {/* <li className='hoverEffect'><a href="#tools">Tools</a></li>
+                <li className='hoverEffect'><a href="#faq">FAQs</a></li> */}
                 </div>
                 <div className="right">
                     <a href="#contact"><div className="btn">Contact Us</div></a>
